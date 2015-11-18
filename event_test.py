d7a4535f38e213a80bfbc75838c0686d827025cd
vassmate <vassmate08@gmail.com>
2015-11-18 11:59:28 +0100

    Added event tests (planned donor number, available beds).

--- event_test.py
+++ event_test.py
@@ -12,6 +12,12 @@ class MyTestCase(unittest.TestCase):
     def test_planned_donor_False2(self):
         self.assertFalse(EventDataValidator.is_valid_planned_donor_number("abc"))
 
+    def test_available_beds_True(self):
+        self.assertTrue(EventDataValidator.is_valid_available_beds("10"))
+
+    def test_available_beds_False(self):
+        self.assertFalse(EventDataValidator.is_valid_available_beds("abc"))
+
 
 if __name__ == '__main__':
     unittest.main()
