ce8633012c452be47abf209dcd3d08b939071723
Bandita69 <aaandris96@gmail.com>
2015-11-19 21:03:00 +0100

    it is gut main.py import if

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-from add_new_donor import AddNewDonor
-from add_new_event import AddNewEvent
-from delete_donor import DeleteDonor
-from delete_event import DeleteEvent
-from list_data import ListData
-from search_data import SearchData
-
 print("----------------------------------------------------------------------- \
       \n--- Welcome to the coolest donor and donation event managing system --- \
       \n-----------------------------------------------------------------------")
@@ -16,22 +9,28 @@ print("\t 1. Add new donor \
     \n \t 5. List donors or donation events \
     \n \t 6. Search \
     \n \t 7. Exit")
-actions = (1, 2, 3, 4, 5, 6, 7)
+actions = ("1", "2", "3", "4", "5", "6", "7")
 action = input("Please choose your action: ")
 while action not in actions:
     print("To exit press 7, otherwise enter a number from 1 to 6.")
     action = input("Please choose your action: ")
 if action == "1":
+    from add_new_donor import AddNewDonor
     AddNewDonor()
 elif action == "2":
+    from add_new_event import AddNewEvent
     AddNewEvent()
 elif action == "3":
+    from delete_donor import DeleteDonor
     DeleteDonor()
 elif action == "4":
+    from delete_event import DeleteEvent
     DeleteEvent()
 elif action == "5":
+    from list_data import ListData
     ListData()
 elif action == "6":
+    from search_data import SearchData
     SearchData()
 elif action == "7":
     exit()
