a5db5b2a9082bcb34480be8aa644907e2a3056d4
Bandita69 <aaandris96@gmail.com>
2015-11-19 13:58:24 +0100

    donor vissza töltve

--- donor.py
+++ donor.py
@@ -1,37 +1,6 @@
 from datetime import datetime
 
 
-<<<<<<< HEAD
-# VALIDATOR CLAS
-class Validator:
-    # NAME
-    def validate_name():
-        result = None
-        userinput = input()
-        for char in userinput:
-            if char.isalpha() or char == " ":
-                result = userinput
-            else:
-                result = False
-        return result
-
-    # GENDER
-    def validate_gender():
-        result = None
-        userinput = input()
-        if len(userinput) == 1:
-            if userinput == "m" or userinput == "f":
-                result = userinput
-            else:
-                result = False
-        else:
-            result = False
-        return result
-
-    # WEIGHT
-    def is_valid_weight():
-        return True
-=======
 # VALIDATOR CLASS
 class DonorValidator:
     # NAME
@@ -47,44 +16,16 @@ class DonorValidator:
     # WEIGHT
     def is_valid_weight(self):
         return str(self).isdigit()
->>>>>>> 2c2305445a01963cca4c046a91dcb2b5ca364b73
 
     # DATE
     def is_valid_date(date):
         try:
-<<<<<<< HEAD
-            valid_date = datetime.strptime(date, "%Y.%m.%d")
-=======
             datetime.strptime(date, "%Y.%m.%d")
->>>>>>> 2c2305445a01963cca4c046a91dcb2b5ca364b73
             return True
         except:
             return False
 
     # SICKNESS
-<<<<<<< HEAD
-    def is_valid_sickness():
-        return True
-
-    # ID NUMBER
-    def is_valid_id_number():
-        return True
-
-    # ID EXPIRATION DATE
-    def is_valid_exp_date():
-        return True
-
-    # BLOOD TYPE
-    def is_valid_blood_type():
-        return True
-
-    # EMAIL ADDRESS
-    def is_valid_email_address():
-        return True
-
-    # MOBILE NUMBER
-    def is_valid_mobile_number():
-=======
     def is_valid_sickness(self):
         yes_or_no = ["y", "n"]
         return str(self).lower() in yes_or_no
@@ -144,66 +85,10 @@ class DonorValidator:
         if not valid_length:
             print("Please enter a mobile number with a valid (11 number) length!")
             return False
->>>>>>> 2c2305445a01963cca4c046a91dcb2b5ca364b73
         return True
 
 
 # INPUT HELPER CLASS
-<<<<<<< HEAD
-class InputHelper:
-    # NAME
-    def get_name():
-        return name
-
-    # GENDER
-    def get_gender():
-        return gender
-
-    # WEIGHT
-    def get_weight():
-        return weight
-
-    # DATE OF BIRTH
-    def get_date_of_birth():
-        date_of_birth = input("Please enter the donor's date of birth in the following format, 2000.12.31: ")
-        v = Validator
-        while not v.is_valid_date(date_of_birth):
-            print("Wrong format. Try again...")
-            date_of_birth = input("Please enter the donor's last donation date in the following format, 2000.12.31: ")
-        return date_of_birth
-
-    def get_donation_date():
-        donation_date = input("Please enter the donor's last donation date in the following format, 2000.12.31: ")
-        v = Validator
-        while not v.is_valid_date(donation_date):
-            print("Wrong format. Try again...")
-            donation_date = input("Please enter the donor's date of birth in the following format, 2000.12.31: ")
-        return donation_date
-
-    # SICKNESS
-    def get_sickness():
-        return sickness
-
-    # ID NUMBER
-    def get_id_number():
-        return id_number
-
-    # ID EXPIRATION DATE
-    def get_exp_date():
-        return exp_date
-
-    # BLOOD TYPE
-    def get_blood_type():
-        return blood_type
-
-    # EMAIL ADDRESS
-    def get_email_address():
-        return email_address
-
-    # MOBILE NUMBER
-    def get_mobile_number():
-        return mobile_number
-=======
 class DonorInputHelper:
     def Input():
         global Input
@@ -284,4 +169,3 @@ class DonorInputHelper:
         while DonorValidator.is_valid_mobile_number(DonorInputHelper.Input()) is False:
             print("M-O-B-I-L-E NUMBER PLS")
         return Input
->>>>>>> 2c2305445a01963cca4c046a91dcb2b5ca364b73
