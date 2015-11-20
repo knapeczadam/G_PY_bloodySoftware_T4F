e8163039454a2473d2ff8a472299117483e4007b
Bandita69 <aaandris96@gmail.com>
2015-11-20 09:33:59 +0100

    add donor javitva

--- add_new_donor.py
+++ add_new_donor.py
@@ -6,44 +6,44 @@ import csv
 
 
 class AddNewDonor:
-	main_do_name = DonorInputHelper.get_name()
-	main_do_gender = DonorInputHelper.get_gender()
-	main_do_weight = DonorInputHelper.get_weight()
-	# If the returned number from the def is not greater than 50, the program stops.
-	if int(main_do_weight) <= 50:
-		print("\nDonors are only accepted above 50 kgs.")
-		print("The program has ended because of not suitable donor.")
-		exit()
-	main_do_birth = DonorInputHelper.get_date_of_birth()
-	# If the returned date from the def is within 18 years of the current date, the program stops.
-	if (datetime.now() - datetime.strptime(main_do_birth, "%Y.%m.%d")).days // 365 < 18:
-		print("\nDonors are only accepted above 18 years.")
-		print("The program has ended because of not suitable donor.")
-		exit()
-	main_do_last_don = DonorInputHelper.get_donation_date()
-	# If the returned date from the def is within 90 days of the current date, the program stops.
-	if (datetime.now() - datetime.strptime(main_do_last_don, "%Y.%m.%d")).days <= 90:
-		print("\nDonors can only give blood once in every 3 months.")
-		print("The program has ended because of not suitable donor.")
-		exit()
-	main_do_sick = DonorInputHelper.get_sickness()
-	if main_do_sick.lower() == "y":
-		print("-----")
-		print("The program has ended because of not suitable donor.")
-		exit()
-	main_do_id = DonorInputHelper.get_id_number()
-	main_do_exp_id = DonorInputHelper.get_exp_date()
-	if datetime.strptime(main_do_exp_id, "%Y.%m.%d") < datetime.now():
-		print("The donor's ID is expired! Program is shutting down...")
-		exit()
-	main_do_blood = DonorInputHelper.get_blood_type()
-	main_do_email = DonorInputHelper.get_email_address()
-	main_do_mobile = DonorInputHelper.get_mobile_number()
-
 
 
     def write_donor_data_infile():
-        
+
+        main_do_name = DonorInputHelper.get_name()
+        main_do_gender = DonorInputHelper.get_gender()
+        main_do_weight = DonorInputHelper.get_weight()
+        # If the returned number from the def is not greater than 50, the program stops.
+        if int(main_do_weight) <= 50:
+            print("\nDonors are only accepted above 50 kgs.")
+            print("The program has ended because of not suitable donor.")
+            exit()
+        main_do_birth = DonorInputHelper.get_date_of_birth()
+        # If the returned date from the def is within 18 years of the current date, the program stops.
+        if (datetime.now() - datetime.strptime(main_do_birth, "%Y.%m.%d")).days // 365 < 18:
+            print("\nDonors are only accepted above 18 years.")
+            print("The program has ended because of not suitable donor.")
+            exit()
+        main_do_last_don = DonorInputHelper.get_donation_date()
+        # If the returned date from the def is within 90 days of the current date, the program stops.
+        if (datetime.now() - datetime.strptime(main_do_last_don, "%Y.%m.%d")).days <= 90:
+            print("\nDonors can only give blood once in every 3 months.")
+            print("The program has ended because of not suitable donor.")
+            exit()
+        main_do_sick = DonorInputHelper.get_sickness()
+        if main_do_sick.lower() == "y":
+            print("-----")
+            print("The program has ended because of not suitable donor.")
+            exit()
+        main_do_id = DonorInputHelper.get_id_number()
+        main_do_exp_id = DonorInputHelper.get_exp_date()
+        if datetime.strptime(main_do_exp_id, "%Y.%m.%d") < datetime.now():
+            print("The donor's ID is expired! Program is shutting down...")
+            exit()
+        main_do_blood = DonorInputHelper.get_blood_type()
+        main_do_email = DonorInputHelper.get_email_address()
+        main_do_mobile = DonorInputHelper.get_mobile_number()
+
 
         data = []
 
