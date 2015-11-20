adda603bd689fcd21f3d5d3aeee438d00e8c24d7
vassmate <vassmate08@gmail.com>
2015-11-20 11:49:18 +0100

    list_event_data kész

--- list_data.py
+++ list_data.py
@@ -3,7 +3,7 @@ import csv
 
 class ListData:
 
-    def list_data():
+    def list__donor_data():
         data = []
 
         with open("Data\donors.csv") as csvfile_read:
@@ -26,4 +26,25 @@ class ListData:
                         or element == dicta[key][2]\
                         or element == dicta[key][3]\
                         or element == dicta[key][9]:
+                    print(element)
+
+    def list_event_data():
+        data = []
+
+        with open("Data\events.csv") as csvfile_read:
+            csvreader = csv.reader(csvfile_read, delimiter=",")
+            for row in csvreader:
+                if row:
+                    data.append(row)
+        csvfile_read.close()
+
+        dicta = {}
+        x = 1
+        for elements in data:
+            dicta[x] = elements
+            x += 1
+
+        for key in dicta:
+            print("\n%s" % key, end=" ")
+            for element in dicta[key]:
                     print(element)
