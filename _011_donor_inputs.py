7b6151d3f3e35aeb4f477564b21cebe4872716b0
knapeczadam <knapecz.adam@gmail.com>
2015-11-26 11:27:29 +0100

    011: 90% done, 012: in progress

--- _011_donor_inputs.py
+++ _011_donor_inputs.py
@@ -4,243 +4,263 @@ GENDERS = ("f", "m")
 ENTER = "Please enter your"
 AGAIN = "Wrong input!"
 SICK = ["y", "n"]
-
+BLOOD = ["a+", "a-", "b+", "b-", "ab+", "ab-", "0+", "0-"]
 
 class Donor:
-    def __init__(self):
-        self.first_name = None
-        self.last_name = None
-        self.gender = None
-        self.weight = None
-        self.date_of_birth = None
-        self.donation_date = None
-        self.sickness = None
-        self.id_number = None
-        self.exp_date = None
-        self.blood_type = None
-        self.email_address = None
-        self.mobile_number = None
+	def __init__(self):
+		self.first_name = None
+		self.last_name = None
+		self.weight = None
+		self.gender = None
+		self.date_of_birth = None
+		self.donation_date = None
+		self.sickness = None
+		self.id_number = None
+		self.exp_date = None
+		self.blood_type = None
+		self.email_address = None
+		self.mobile_number = None
 
-    def get_first_name(self):
-        """
+	def get_first_name(self):
+		"""
 
-        :return:
-        """
-        self.first_name = input("{} first name: ".format(ENTER))
-        while Donor.is_valid_name(self.first_name) is False:
-            self.first_name = input("{}\nThe first name can contain only letters and has to be at least 2 \
-            characters long!\n{} first name: ".format(AGAIN, ENTER))
-        return self.first_name
+		:return:
+		"""
+		self.first_name = input("{} first name: ".format(ENTER))
+		while Donor.is_valid_name(self.first_name) is False:
+			self.first_name = input("{}\nThe first name can contain only letters and has to be at least 2 \
+			characters long!\n{} first name: ".format(AGAIN, ENTER))
+		return self.first_name
 
-    def get_last_name(self):
-        """
+	def get_last_name(self):
+		"""
 
-        :return:
-        """
-        self.last_name = input("{} last name: ".format(ENTER))
-        while Donor.is_valid_name(self.last_name) is False:
-            self.last_name = input("{}\nThe last name can contain only letters and has to be at least 2 \
-            characters long!\n{} last name: ".format(AGAIN, ENTER))
-        return self.last_name
+		:return:
+		"""
+		self.last_name = input("{} last name: ".format(ENTER))
+		while Donor.is_valid_name(self.last_name) is False:
+			self.last_name = input("{}\nThe last name can contain only letters and has to be at least 2 \
+			characters long!\n{} last name: ".format(AGAIN, ENTER))
+		return self.last_name
 
-    def is_valid_name(name):
-        """
+	def is_valid_name(name):
+		"""
 
-        :return:
-        """
-        return name.isalpha() and len(name) > 1
+		:return:
+		"""
+		return name.isalpha() and len(name) > 1
 
-    def get_gender(self):
-        """
+	def get_weight(self):
+		"""
 
-        :return:
-        """
-        self.gender = input("Please enter an F if you are a Female or an M if you are a Male: ")
-        while Donor.is_valid_gender(self.gender) is False:
-            self.gender = input("{}\nYou can only choose the letter F or M! \
-            \nPlease enter an F if you are a Female or an M if you are a Male: ".format(AGAIN))
-        return self.gender
+		:return:
+		"""
+		self.weight = input("{} weight: ".format(ENTER))
+		while Donor.is_valid_weight(self.weight) is False:
+			self.weight = input("{}\nThe weight has to be a positive number\n{} weight: ".format(AGAIN, ENTER))
+		return self.weight
 
-    def is_valid_gender(gender):
-        """
+	def is_valid_weight(weight):
+		"""
 
-        :return:
-        """
-        return gender.lower() in GENDERS
+		:return:
+		"""
+		return str(weight).isdigit() and int(weight) > 0
 
-    def get_weight(self):
-        """
+	def get_gender(self):
+		"""
 
-        :return:
-        """
-        self.weight = input("{} weight: ".format(ENTER))
-        while Donor.is_valid_weight(self.weight) is False:
-            self.weight = input("{}\nThe weight has to be a positive number\n{} weight: ".format(AGAIN, ENTER))
-        return self.weight
+		:return:
+		"""
+		self.gender = input("Please enter an F if you are a Female or an M if you are a Male: ")
+		while Donor.is_valid_gender(self.gender) is False:
+			self.gender = input("{}\nYou can only choose the letter F or M! \
+			\nPlease enter an F if you are a Female or an M if you are a Male: ".format(AGAIN))
+		return self.gender
 
-    def is_valid_weight(weight):
-        """
+	def is_valid_gender(gender):
+		"""
 
-        :return:
-        """
-        return str(weight).isdigit() and int(weight) > 0
+		:return:
+		"""
+		return gender.lower() in GENDERS
 
-    def get_date_of_birth(self):
-        """
+	def get_date_of_birth(self):
+		"""
 
-        :return:
-        """
-        self.date_of_birth = input("{} date of birth in the following format, 2000.12.31: ".format(ENTER))
-        while Donor.is_valid_date(self.date_of_birth) is False:
-            self.date_of_birth = input(
-                "{}\n{} date of birth in the following format, 2000.12.31: ".format(AGAIN, ENTER))
-        return self.date_of_birth
+		:return:
+		"""
+		self.date_of_birth = input("{} date of birth in the following format, 2000.12.31: ".format(ENTER))
+		while Donor.is_valid_date(self.date_of_birth) is False:
+			self.date_of_birth = input(
+				"{}\n{} date of birth in the following format, 2000.12.31: ".format(AGAIN, ENTER))
+		return self.date_of_birth
 
-    def is_valid_date(date):
-        """
+	def is_valid_date(date):
+		"""
 
-        :return:
-        """
-        try:
-            datetime.strptime(date, "%Y.%m.%d")
-            return True
-        except:
-            return False
+		:return:
+		"""
+		try:
+			datetime.strptime(date, "%Y.%m.%d")
+			return True
+		except:
+			return False
 
-    def get_donation_date(self):
-        """
+	def get_donation_date(self):
+		"""
 
-        :return:
-        """
-        self.donation_date = input("If you have donated blood before, please enter its date \
-        in the following format 2000.12.31 otherwise press ENTER: ")
-        while Donor.is_valid_date(self.donation_date) is False:
-            self.donation_date = input("{}\nIf you have donated blood before, please enter its date \
-        in the following format 2000.12.31 otherwise press Enter: ".format(AGAIN))
-        return self.donation_date
+		:return:
+		"""
+		self.donation_date = input("If you have donated blood before, please enter its date \
+		in the following format 2000.12.31 otherwise press ENTER: ")
+		while Donor.is_valid_date(self.donation_date) is False:
+			self.donation_date = input("{}\nIf you have donated blood before, please enter its date \
+		in the following format 2000.12.31 otherwise press Enter: ".format(AGAIN))
+		return self.donation_date
 
-    def is_valid_donation_date(date):
-        """
+	def is_valid_donation_date(date):
+		"""
 
-        :return:
-        """
-        try:
-            if date == "":
-                return True
-            datetime.strptime(date, "%Y.%m.%d")
-            return True
-        except:
-            return False
+		:return:
+		"""
+		try:
+			if date == "":
+				return True
+			datetime.strptime(date, "%Y.%m.%d")
+			return True
+		except:
+			return False
 
-    def get_sickness(self):
-        """
+	def get_sickness(self):
+		"""
 
-        :return:
-        """
-        self.sickness = input("Were you sick in the last month? For yes press Y for no press N: ")
-        while Donor.is_valid_sickness(self.sickness) is False:
-            self.sickness = input("{} Press either Y or N: ".format(AGAIN))
-        return self.sickness
+		:return:
+		"""
+		self.sickness = input("Were you sick in the last month? For yes press Y for no press N: ")
+		while Donor.is_valid_sickness(self.sickness) is False:
+			self.sickness = input("{} Press either Y or N: ".format(AGAIN))
+		return self.sickness
 
-    def is_valid_sickness(sickness):
-        """
+	def is_valid_sickness(sickness):
+		"""
 
-        :return:
-        """
-        return sickness.lower() in SICK
+		:return:
+		"""
+		return sickness.lower() in SICK
 
-    def get_exp_date(self):
-        print("Please enter your ID experiment date:")
-        while Donor.is_valid_date() is False:
-            print("Try again!:")
-        return Input
+	def get_id_number(self):
+		"""
 
-    def is_valid_id_number(self):
+		:return:
+		"""
+		self.id_number = input("{} ID : ".format(ENTER))
+		while Donor.is_valid_date(self.id_number) is False:
+			self.id_number = input("{}  ".format(AGAIN, ENTER))
+		return self.id_number
 
-        if len(str(self)) != 8 or not str(self).isalnum():
-            return False
-        if str(self)[:6].isdigit() and str(self)[6:].isalpha():
-            print("Your ID number is recorded.")
-            return True
-        elif str(self)[:2].isalpha() and str(self)[2:].isdigit():
-            print("Your passport number is recorded.")
-            return True
-        else:
-            print("Not ID or Pass, But recorded.")
-            return True
+	def is_valid_id_number(id_number):
+		"""
 
-    # BLOOD TYPE
-    # TDD VAN
-    def is_valid_blood_type(self):
-        Blood_types = ["a", "b", "ab", "0"]
-        return str(self).lower() in Blood_types
+		:return:
+		"""
+		if len(id_number) != 8 or not id_number.isalnum():
+			return False
+		if id_number[:6].isdigit() and id_number[6:].isalpha():
+			print("Your ID number is recorded.")
+			return True
+		elif id_number[:2].isalpha() and id_number[2:].isdigit():
+			print("Your passport number is recorded.")
+			return True
+		else:
+			print("Not ID or passport but recorded.")
+			return True
 
-    # EMAIL ADDRESS
-    # TDD VAN
-    def is_valid_email_address(self):
-        email_address_string = str(self).replace(" ", "")
+	def get_exp_date(self):
+		"""
 
-        contains_at_sign = "@" in email_address_string and email_address_string.index("@") > 0
-        ending_is_valid = email_address_string.endswith(".hu") or email_address_string.endswith(".com")
+		:return:
+		"""
+		self.exp_date = input("{} ID expiration date in the following format 2000.12.31: ".format(ENTER))
+		while Donor.is_valid_date(self.exp_date) is False:
+			self.exp_date = input("{} {} ID expiration date in the following format 2000.12.31: ".format(AGAIN, ENTER))
+		return self.exp_date
 
-        if not contains_at_sign:
-            print("Please add an '@' sign in your address!")
-            return False
-        if not ending_is_valid:
-            print("Please specify where your email provider is ('.com' or '.hu')!")
-            return False
-        return True
+	def get_blood_type(self):
+		"""
 
-    # MOBILE NUMBER
-    # TDD VAN
-    def is_valid_mobile_number(self):
-        mobile_string = str(self).replace(" ", "")
+		:return:
+		"""
+		self.blood_type = input("{} type of blood: ".format(ENTER))
+		while Donor.is_valid_blood_type(self.blood_type) is False:
+			self.blood_type = input("{} {} type of blood: ".format(AGAIN, ENTER))
+		return self.blood_type
 
-        start_36_or_06 = mobile_string.startswith("+36") or mobile_string.startswith("06")
+	def is_valid_blood_type(blood_type):
+		"""
 
-        contains_20_30_70 = "20" in mobile_string or "30" in mobile_string \
-                            or "70" in mobile_string
-        valid_length = len(mobile_string) == 11
+		:return:
+		"""
+		return blood_type.lower() in BLOOD
 
-        if not start_36_or_06:
-            print("Please specify your number and add '36' or '06' at the beginning!")
-            return False
-        if not contains_20_30_70:
-            print("Please specify your provider: 20/30/70 !")
-            return False
-        if not valid_length:
-            print("Please enter a mobile number with a valid (11 number) length!")
-            return False
-        return True
+	def get_email_address(self):
+		"""
 
+		:return:
+		"""
+		self.email_address = input("{} email: ".format(ENTER))
+		while Donor.is_valid_email_address is False:
+			self.email_address = input("{} ".format(AGAIN))
+		return self.email_address
 
-    # ID NUMBER
-    def get_id_number():
-        print("Please enter your ID:")
-        while DonorValidator.is_valid_id_number(DonorInputHelper.Input()) is False:
-            print("Try again!:")
-        return Input
+	def is_valid_email_address(email_address):
+		"""
 
-    # ID EXPIRATION DATE
+			:rtype: object
+			:return:
+			"""
+		email_address = email_address.replace(" ", "")
+		if "@" not in email_address and email_address.index("@") > 0:
+			print("Please add an '@' sign in your address!")
+			return False
+		if not email_address.endswith(".hu") or email_address.endswith(".com"):
+			print("Please specify where your email provider is ('.com' or '.hu')!")
+			return False
+		at_sign_number = 0
+		for letter in email_address:
+			if letter == "@":
+				at_sign_number += 1
+				if at_sign_number > 1:
+					print("Email address contains only 1 at sign!")
+					return False
+		return True
 
+	def get_mobile_number(self):
+		"""
 
-    # BLOOD TYPE
-    def get_blood_type():
-        print("Please enter your type of blood:")
-        while DonorValidator.is_valid_blood_type(DonorInputHelper.Input()) is False:
-            print("Try again!:")
-        return Input
+		:return:
+		"""
+		self.mobile_number = input("{} mobile number: ".format(ENTER))
+		while Donor.is_valid_mobile_number(self.mobile_number) is False:
+			self.mobile_number = input("{} ".format(AGAIN))
+		return self.mobile_number
 
-    # EMAIL ADDRESS
-    def get_email_address():
-        print("Please enter your email:")
-        while DonorValidator.is_valid_email_address(DonorInputHelper.Input()) is False:
-            print("Try again!:")
-        return Input
+	def is_valid_mobile_number(mobile_number):
+		"""
 
-    # MOBILE NUMBER
-    def get_mobile_number():
-        print("Please enter your mobile number:")
-        while DonorValidator.is_valid_mobile_number(DonorInputHelper.Input()) is False:
-            print("Try again!:")
-        return Input
+		:return:
+		"""
+		mobile_number = mobile_number.replace(" ", "")
+		if not (mobile_number[:12].isdigit() or mobile_number[1:13].isdigit()):
+			print()
+			return False
+		if not (mobile_number.startswith('06') or mobile_number.startswith('+36')):
+			print("Please specify your number and add '36' or '06' at the beginning!")
+			return False
+		if not ((mobile_number[3:5] in ("20", "30", "70")) or (mobile_number[2:4] in ("20", "30", "70"))):
+			print("Please specify your provider: 20/30/70 !")
+			return False
+		if not (len(mobile_number) == 11 or len(mobile_number) == 12):
+			print("Please enter a mobile number with a valid (11 number) length!")
+			return False
+		return True
