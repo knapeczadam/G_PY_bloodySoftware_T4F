5e34d100b9b575ffc8f12732478ae4905e72e0ee
Bandita69 <aaandris96@gmail.com>
2015-12-02 14:27:46 +0100

    Event szörcs added

--- _060_search_data.py
+++ _060_search_data.py
@@ -34,11 +34,22 @@ def search_donors(new_list, file_name):
 	if file_name == "Data\events.csv":
 		searched_event_data = input("Please enter the event's data for search: ")
 		for row in new_list:
-			for data in row:
-				if searched_event_data in data and row != new_list[0]:
-					print(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10])
+			for element in row:
+				if searched_event_data in element and row != new_list[0]:
 					found_donor_or_event += 1
-					break
+					row_counter += 1
+					if row_counter % 3 == 0 and row_counter != 0:
+						print(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10])
+						answer = input("\nNext page? Press y to continue: ")
+						if answer == 'y':
+							os.system("CLS")
+							row_counter = 0
+							break
+					else:
+						if row_counter != 0:
+
+							print(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10])
+							break
 
 	if found_donor_or_event == 0:
 		print("Given data is not found!")
